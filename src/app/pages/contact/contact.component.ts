c4df16cf3ea20407eb7e765950e9a4a9703771ef
julia shevchenko <julia.shevchenko@dataart.com>
2024-09-25 16:30:42 +0300

    Reactive form - Part 2.

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,25 +1,42 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormArray, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatGridListModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
   profileForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    aliases: new FormArray([new FormControl('')]),
   });
 
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(new FormControl(''));
+    // this.profileForm.value.aliases?.push('');
+  }
+
+  deleteAlias(index: number) {
+    this.aliases.removeAt(index);
+    // this.profileForm.value.aliases?.push('');
+  }
+
   handleSubmit() {
-    alert(
-      this.profileForm.value.name + ' | ' + this.profileForm.value.email
-    );
+    // alert(
+    //   this.profileForm.value.name + ' | ' + this.profileForm.value.email
+    // );
+    console.warn(this.profileForm.value);
   }
 }
